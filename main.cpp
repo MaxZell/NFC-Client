8f46624c80c33494419c913225ba5008b882ad29
mzelensky <maxim.zelensky@soxes.ch>
2021-03-26 13:54:37 +0100

    oled messages + http update

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
     @autors: Maxim Zelensky & Jason Banyer
     @date: 12.03.2021
     @version: 
-    NFC Sensor gets UID and send to Node backend -> will be saved in MongoDB
+    @desctiption: NFC Sensor gets UID and send to Node backend -> will be saved in MongoDB
     @todo: change wifi pref.
 */
 #include "mbed.h"
@@ -28,28 +28,35 @@ int main()
     // OLED Display
     oled.clear();
     printf("RFID HTTP\n");
+    oled.printf("RFID HTTP\n");
     rfidReader.PCD_Init();
 
     //connect to wifi
+    oled.printf("wifi connection...\n");
     WiFiInterface* network = WiFiInterface::get_default_instance();
     if (!network) {
         printf("ERROR: No WiFiInterface found.\n");
+        oled.printf("ERROR: No WiFiInterface found.\n");
         return -1;
     }
     printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
     int ret = network->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
     if (ret != 0) {
         printf("\nConnection error: %d\n", ret);
+        oled.printf("\nConnection error: %d\n", ret);
         return -1;
     }
-    printf("Success\n\n");
+    printf("Success wifi conn\n\n");
+    oled.printf("Success wifi conn\n\n");
     printf("MAC: %s\n", network->get_mac_address());
     SocketAddress a;
     network->get_ip_address(&a);
     printf("IP: %s\n", a.get_ip_address());  
     //end of wifi connect
 
-    while   ( 1 ) 
+    oled.clear();
+    oled.printf("Attach NFC-Card\n");
+    while   ( 1 )
     {
         // RFID Reader
         if ( rfidReader.PICC_IsNewCardPresent())
@@ -61,24 +68,30 @@ int main()
                 for ( int i = 0; i < rfidReader.uid.size; i++ )
                     printf("%02X:", rfidReader.uid.uidByte[i]);
                     //send uid to backend
-                    HttpRequest* get_req = new HttpRequest(network, HTTP_GET, "https://m242-backend.herokuapp.com/ajax/get-data");
+                    HttpRequest* get_req = new HttpRequest(network, HTTP_GET, "http://m242-backend.herokuapp.com/ajax/get-data");
+                    oled.printf("pre send\n");
                     HttpResponse* get_res = get_req->send();
+                    oled.printf("sending request...\n");
+
                     //success response
                     if (get_res){
                         printf( "request: %s\n", (char*) get_res->get_body() );
+                        oled.printf( "request: %s\n", (char*) get_res->get_body() );
                         printf("SUCCESS REQUEST");
+                        oled.printf("SUCCESS REQUEST\n");
                     }
                     // Error
                     else{
                         printf("HttpRequest failed (error code %d)\n", get_req->get_error());
+                        oled.printf("HttpRequest failed (error code %d)\n", get_req->get_error());
                         return 1;
                     }
                     delete get_req;
 
-                printf("\r\n");
-                // Print Card type
-                int piccType = rfidReader.PICC_GetType(rfidReader.uid.sak);
-                printf("PICC Type: %s \r\n", rfidReader.PICC_GetTypeName(piccType) );
+                // printf("\r\n");
+                // // Print Card type
+                // int piccType = rfidReader.PICC_GetType(rfidReader.uid.sak);
+                // printf("PICC Type: %s \r\n", rfidReader.PICC_GetTypeName(piccType) );
                 
             }
         thread_sleep_for( 200 );
